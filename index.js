02b499e3f6c8269b0668fbd85db80132ee67de38
Fabrício Barretti <fabriciobarretti@gmail.com>
2023-11-26 15:09:22 -0300

    HTTP forms

---
+++ index.js
@@ -0,0 +1,11 @@
+//Getting DOMs for clients and buttons
+let clientName = document.getElementById("client-name-input");
+let clientAge = document.getElementById("age-input");
+let clientPhone = document.getElementById("phone-input");
+let clientCity = document.getElementById("city-input");
+let clientCountry = document.getElementById("country-input");
+let clientStatus = document.getElementById("status-input");
+
+let addButton = document.getElementById("add-btn");
+let showAllClientsButton = document.getElementById("show-all-clients-btn");
+
