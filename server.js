b90185e8381922c47ed533934df37ca086d1bcfe
Fabrício Barretti <fabriciobarretti@gmail.com>
2023-11-28 14:20:22 -0300

    Small changes

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require ("express");
 const mysql = require ("mysql2");
 const PORT = 3302;
+const bodyParser = require('body-parser');
 // const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false}));
 // app.use('/', routes);
 
 app.listen(PORT, () => {
