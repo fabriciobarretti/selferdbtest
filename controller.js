e7f7909afc40e4054cbc53565d6c440e4e00a397
Fabrício Barretti <fabriciobarretti@gmail.com>
2023-12-04 16:11:49 -0300

    Minor changes

--- controller.js
+++ controller.js
@@ -35,19 +35,15 @@ module.exports = {
         let clientlist = {error: '', result:{}};
         let showing = await queries.showClients();
         clientlist.result = showing;
-        // res.json(clientlist.result);
-        
-        // Pegar o nome de cada key e jogar numa array
-        // let dataLength = Object.keys(showing);
+
         let dataKeys = Object.keys(showing[0]);
 
         console.log(showing);
         console.log(dataKeys);
+
+        for (i=0;i<dataKeys.length;i++){
+                dataKeys[i] = dataKeys[i].charAt(0).toUpperCase() + dataKeys[i].slice(1);
+        }
         res.render('clients', { data: clientlist.result, dataKeys: dataKeys});
     }
-
-    // createTable: async (req,res) => {
-    //     let json = {error:'', result: {}};
-
-    // }
 };
--- queries.js
+++ queries.js
@@ -35,34 +35,3 @@ module.exports = {
       })
     }
   };
-
-// Old code used for creating the database and table.
-// The code used here came from: https://www.simplilearn.com/tutorials/nodejs-tutorial/nodejs-mysql
-
-//   // Creating DB
-// app.get("/createdb", (req, res) => {
-//     let sql = "CREATE DATABASE selferdbtest";
-  
-//     db.query(sql, (err) => {
-//       if (err) {
-//         throw err;
-//       }
-    
-//     // res.send("Database created");
-//     console.log("Database created.");
-//     });
-//   });
-
-// // Creating table
-// app.get("/createtable", (req, res) => {
-//     let sql = "CREATE TABLE clients(id int AUTO_INCREMENT, name VARCHAR(255), age VARCHAR(255), phone VARCHAR(255), city VARCHAR(255), country VARCHAR(255), status BINARY PRIMARY KEY(id))";
-  
-//     db.query(sql, (err) => {
-//       if (err) {
-//         throw err;
-//       }
-  
-//       // res.send("Employee table created");
-//       console.log("Clients table created");
-//     });
-//   });
